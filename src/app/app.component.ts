4aad8a24d39912366a5aade70d700050f123f81e
cse-t-kutsuna <cse_t_kutsuna@incrementp.co.jp>
2020-08-20 17:44:50 +0900

    feat(app.component): add simple mgl-map.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mapbox-universal';
+  style: any = {
+    sources: {
+      world: {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json'
+      }
+    },
+    version: 8,
+    layers: [{
+      id: 'countries',
+      type: 'fill',
+      source: 'world',
+      layout: {},
+      paint: {
+        'fill-color': '#6F788A'
+      }
+    }]
+  };
 }
