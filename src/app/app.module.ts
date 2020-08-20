8ed85d92ff4d0ea06b6403cf52ba2334f2735101
cse-t-kutsuna <cse_t_kutsuna@incrementp.co.jp>
2020-08-20 17:42:01 +0900

    feat(app.module, styles): add NgxMapboxGLModule and css.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    NgxMapboxGLModule,
     AppRoutingModule
   ],
   providers: [],
